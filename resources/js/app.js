f945f72a2be8e8255661d71412619b1c05f3877d
nadia-art <nadiaalahyane9@gmail.com>
2023-02-13 10:12:40 +0100

    second commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+
+import {createApp} from 'vue';
+
+import App from './components/Calculator.vue';
+
+createApp(App).mount("#app");
---
+++ resources/views/calculator.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</head>
+
+<body>
+    <h1 style="text-align: center;">Simple calculator with laravel 9 and vuejs3</h1>
+    <div id="app"></div>
+
+    @vite('resources/js/app.js')
+</body>
+
+</html>
